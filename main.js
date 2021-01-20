311f84eb08bc0792884631b185399932c5c153cd
Ging123 <iceglauber@outlook.com>
2021-01-20 14:45:56 -0300

    adjuste and put the videos container objects

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
 const youtubeVideo = {
-  videoImageSrc: ["https://i.ytimg.com/vi/PeCbEGzy6i8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCSBUZX_FyuRAm_bl_86J8tl_i2ew"
-  ],
-  videoTitle:["Sonic chill musics"],
-  channelName:["Zequinha games"],
-  channelImage:["https://yt3.ggpht.com/ytc/AAUvwnh8ofZQlcMov1-2sGtvlnINYkG7yA127u77nBXs-Q=s68-c-k-c0x00ffffff-no-rj"
-  ]
+  videoImageSrc: ["https://i.ytimg.com/vi/PeCbEGzy6i8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rsAOn4CLCSBUZX_FyuRAm_bl_86J8tl_i2ew=AOn4CLCSBUZX_FyuRAm_bl_86J8tl_i2ew",
+  "https://i.ytimg.com/vi/qTXZpZhgoBo/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA-srRG-8HX3YxMQQimavht9Muw7w",
+  "https://i.ytimg.com/vi/m_mU7kp-JbU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBgsfWcCOfnQNlyfF9EqeQjvmaLvQ", 
+  "https://i.ytimg.com/vi/SMZg38xOlhY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB-a4qJEKTHMRO28AhG_eQLl_uIEQ"
+, "https://i.ytimg.com/vi/lsHCzboWK0U/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBg9h9yqIxZRsINNb4pTyYVuL6Q5Q"
+, "https://i.ytimg.com/an_webp/H-KVHiFKxSw/mqdefault_6s.webp?du=3000&sqp=CNj1oIAG&rs=AOn4CLBDCqCDAtsaSsRl-fowxgLDkLhnPw"
+, "https://i.ytimg.com/vi/90Sq7EtQobE/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAoK0Mc8sQI-Ek7dKgi1I0AB6ndoA"
+, "https://i.ytimg.com/vi/kkYaQ3FR7hc/hqdefault.jpg?sqp=-oaymwEZCOADEI4CSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC_OfcyY9DR-fjBspl6jMG2Ra99QQ"
+, "https://i.ytimg.com/vi/hj83cwfOF3Y/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDAALQ5a_aLyR93cSkAaVWmun8BkQ"
+, "https://i.ytimg.com/vi/2MtOpB5LlUA/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC4BOShsL9iHoXcyXcDhRdhRdlm_w"
+, "https://i.ytimg.com/vi/DlyXajYv67U/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAfIqSC49fSRxPUTrRbPi6cs_lziA"
+, "https://i.ytimg.com/vi/W5AZeNGB6Ds/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBq1IjffCnkyi6Pg_Ki5vIVr8rlPw"
+, "https://i.ytimg.com/vi/_ham4rIOM34/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC6zJlmb9hZCu6hiBDVdgSdm91cWg"
+, "https://i.ytimg.com/an_webp/ZSWzPI4BqvA/mqdefault_6s.webp?du=3000&sqp=CJC9oYAG&rs=AOn4CLA2sykqPvqDppqKxym0Yl7zkBTx9Q"
+, "https://i.ytimg.com/vi/wDgQdr8ZkTw/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBRQWVMV3ivU-O7qptH0UFGGtD-cw"
+, "https://i.ytimg.com/vi/2ye8iNTmvqg/hqdefault.jpg?sqp=-oaymwEZCOADEI4CSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCka3n52726mmYNFSYoEYcFQJbxgg"],
+
+  videoTitle: ["Sonic chill musics", "New Metroid Game", "the most badass mario character", "Songs to relax",
+  "top 10 animes fight", "Fairy Tail ep 40", "Battle arena toshiden 3 gameplay", "Dragon Ball z ep 243",
+  "Musics for relax and sleep", "is jojo a good anime ?", "how to draw kirby", "the best game of all times",
+  "all sonic colors cutscenes", "the best genesis game", "Undertale - Megalovania",
+  "best scenes of hokuto no ken"],
+
+  channelName: ["Zequinha games", "CarlãoONerd", "SonicFanBoy800", "trumpAndBidenKiss2038", "jackHandson999",
+  "theVirginNerd23", "good games.com", "baldBallZ", "ILikeFingers29", "Jotaro222", "TheDrawMan", 
+  "marioGamer", "sonicBoomTrash", "megaNintendoPlayX", "sans939", "youAlredyDead44"],
+
+  channelImage: ["https://yt3.ggpht.com/ytc/AAUvwnh8ofZQlcMov1-2sGtvlnINYkG7yA127u77nBXs-Q=s68-c-k-c0x00ffffff-no-rj",
+    "https://yt3.ggpht.com/ytc/AAUvwnjrFKg-h_y8WH65kHoa0qVci9Pd_kwSzrDDjFBGHA=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwnjfPUuLOq1SaLcxAGpP5FxeqsGMkvHDgGqEUw=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwnhfcM_W_pqrwEiRZMb7OesgGZQmeksthUd-VwJCXg=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwnhCL3plZ_7M5Uw-xLJYHIfUEM-wnGt-ctLYlbOpUw=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwniSUFlILvLWJ9bJD9cXiqg8Z8Rokjl_N5GzrLbzhQ=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwnj7CiuWEaVrdiFnNVfo04xAfBP803WbCemCgg=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwngozRxqxRxNdNk-Cp0Uc4AETiYwtWiJAvV-fObJ0w=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwnhANFZdwN2Q4Wey3kkapZLhaj5aOBLnORHcXnaH=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwniHe63kOEBOhQYjQ54m34YXYQIxs-3qVGzNWF8XSQ=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwngKrVxPHY4d7DO1r1cne2_Tu8wRF2E_lfpKlUdUig=s68-c-k-c0x00ffffff-no-rj" 
+  , "https://yt3.ggpht.com/ytc/AAUvwngUo5P-AUOCXemhGvbq_Ylt1qC6NueTFJCl2C9hDA=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwnjDtMksj0XvTHK3njovRheQc3ZhMV7sI98U-JSXpQ=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwniWzvQQsxPWA-Zynter95N_DsgE22wm9lFw1nyj7Q=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwni53TyAPBge8v2C8z-CdtVQtJvEXWaiLbkSRg=s68-c-k-c0x00ffffff-no-rj"
+  , "https://yt3.ggpht.com/ytc/AAUvwnh_-mztXCa0VgTD199wM9FlcX1y3bdeLRmctNxN=s68-c-k-c0x00ffffff-no-rj"]
 }
 
 //FUNCTION TO CREATE ARRAYS
 function combineArray(array1, array2) {
   let arrayCombined = new Array(array1.length);
-  for(let i = 0; i < array1.length; i++) {
-    arrayCombined[i] = `${array1[i]}${array2}`;
+  for (let i = 0; i < array1.length; i++) {
+    arrayCombined[i] = `${array1[i]}${array2[i]}`;
   }
   return arrayCombined;
 }
@@ -25,11 +62,11 @@ function returnArrayOfTimeOfVideo(len = 0) {
     durationIndex = Math.floor(Math.random() * 3);
     if (duration[durationIndex] === "short") {
       arrayOfTime[i] = createARandomTime();
-    } 
-    else if(duration[durationIndex] === "medium") {
+    }
+    else if (duration[durationIndex] === "medium") {
       arrayOfTime[i] = createARandomTime(true);
-    } 
-    else if(duration[durationIndex] === "long") {
+    }
+    else if (duration[durationIndex] === "long") {
       arrayOfTime[i] = createARandomTime(true, true);
     }
   }
@@ -57,9 +94,9 @@ function createARandomTime(hasMinutes = false, hasHours = false) {
 
 function putAZeroInFrontOfAllIntThatIsLessThanNine(...value) {
   const newValue = new Array(value.length);
-  for(let i = 0; i < value.length; i++) {
+  for (let i = 0; i < value.length; i++) {
     newValue[i] = value[i];
-    if(value[i] <= 9) {
+    if (value[i] <= 9) {
       newValue[i] = `0${value[i]}`;
     }
   }
@@ -71,15 +108,15 @@ function returnArrayOfViews(len = 0) {
   const arrayOfViews = new Array(len);
   const howMuchViews = ["fewViews", "mediumViews", "aLotOfViews"];
   let index = 0;
-  for(let i = 0; i < len; i++) {
+  for (let i = 0; i < len; i++) {
     index = Math.floor(Math.random() * 3);
     if (howMuchViews[index] === "fewViews") {
-      arrayOfViews[i] = `${Math.floor(Math.random() * 999 + 1)}views • `;
-    } 
-    else if(howMuchViews[index] === "mediumViews") {
+      arrayOfViews[i] = `${Math.floor(Math.random() * 999 + 1)} views • `;
+    }
+    else if (howMuchViews[index] === "mediumViews") {
       arrayOfViews[i] = `${Math.floor(Math.random() * 999 + 1)}K views • `;
-    } 
-    else if(howMuchViews[index] === "aLotOfViews") {
+    }
+    else if (howMuchViews[index] === "aLotOfViews") {
       arrayOfViews[i] = `${Math.floor(Math.random() * 999 + 1)}M views • `;
     }
   }
@@ -91,15 +128,15 @@ function returnArrayOfDataOfTheVideoRelease(len = 0) {
   const arrayOfTheData = new Array(len);
   const howMuchTimePassSinceTheRelease = ["few", "medium", "much"];
   let index = 0;
-  for(let i = 0; i < len; i++) {
+  for (let i = 0; i < len; i++) {
     index = Math.floor(Math.random() * 3);
     if (howMuchTimePassSinceTheRelease[index] === "few") {
       arrayOfTheData[i] = `${Math.floor(Math.random() * 3 + 1)} week ago`;
-    } 
-    else if(howMuchTimePassSinceTheRelease[index] === "medium") {
+    }
+    else if (howMuchTimePassSinceTheRelease[index] === "medium") {
       arrayOfTheData[i] = `${Math.floor(Math.random() * 11 + 1)} month ago`;
-    } 
-    else if(howMuchTimePassSinceTheRelease[index] === "much") {
+    }
+    else if (howMuchTimePassSinceTheRelease[index] === "much") {
       arrayOfTheData[i] = `${Math.floor(Math.random() * 99 + 1)} year Ago`;
     }
   }
@@ -153,7 +190,7 @@ function createAndPutTheVideosContainerOnHtml() {
   const len = youtubeVideo.videoImageSrc.length;
   const videoContainer = createANewHtmlElement("div", "videoContainer", len, "");
   appendHtmlElementOnAnother("#gridOfYoutubeVideos", true, ...videoContainer);
-  
+
   putImageOfTheVideoInTheContainer(len);
   putTimeOnTheImageOfTheVideo(len);
   putTheVideosInfo(len);
@@ -161,8 +198,9 @@ function createAndPutTheVideosContainerOnHtml() {
 
 
 function putImageOfTheVideoInTheContainer(len = 0) {
+  const videoImagesArray = youtubeVideo.videoImageSrc;
   const videoImageContainer = createANewHtmlElement("div", "videoImageContainer", len, "");
-  const videoImgSrc = createAImgElement("videoImage", len, youtubeVideo.videoImageSrc);
+  const videoImgSrc = createAImgElement("videoImage", len, ...videoImagesArray);
   appendHtmlElementOnAnother(".videoContainer", false, ...videoImageContainer);
   appendHtmlElementOnAnother(".videoImageContainer", false, ...videoImgSrc);
   putIconesOnTheVideoImg(len);
@@ -196,14 +234,17 @@ function putTimeOnTheImageOfTheVideo(len = 0) {
 
 
 function putTheVideosInfo(len = 0) {
+  const videoTitleArray = youtubeVideo.videoTitle;
+  const channelNameArray = youtubeVideo.channelName;
+  const chanellImageArray = youtubeVideo.channelImage;
   const videoInfosContainer = createANewHtmlElement("div", "videosInfos", len, "");
-  const channelImage = createAImgElement("channelImage", len, youtubeVideo.channelImage);
+  const channelImage = createAImgElement("channelImage", len, ...chanellImageArray);
   const i = createANewHtmlElement("i", "fas fa-ellipsis-v addToQueue", len, "");
 
   const infosOfTheVideo = createANewHtmlElement("div", "infos", len, "");
-  const videoTitle = createANewHtmlElement("div", "videoTitle", len, youtubeVideo.videoTitle);
-  const channelName = createANewHtmlElement("div", "channelName", len, youtubeVideo.channelName);
-  
+  const videoTitle = createANewHtmlElement("div", "videoTitle", len, ...videoTitleArray);
+  const channelName = createANewHtmlElement("div", "channelName", len, ...channelNameArray);
+
   appendHtmlElementOnAnother(".videoContainer", false, ...videoInfosContainer);
   appendHtmlElementOnAnother(".videosInfos", false, ...channelImage);
   appendHtmlElementOnAnother(".videosInfos", false, ...infosOfTheVideo);
