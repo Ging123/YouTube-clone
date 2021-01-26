b93864f518764cbe68b191dc4ad7164899169179
Ging123 <iceglauber@outlook.com>
2021-01-26 16:24:45 -0300

    create a smaller hover for responsivite

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # YouTube-clone
+This is a code that I write for train htmls, css and js
+I tried to copy the youtube home page
+I hope that you enjoy it
--- main.js
+++ main.js
@@ -1,10 +1,25 @@
+var bigHoverIsVisible = true;
+
+var SmallLeftHoverOptions = {
+  classes:["fas fa-home", "fas fa-fire", "fas fa-tv", "fas fa-book-open"],
+  iconesName:["Home", "Trending", "Subscriptions", "Library"]
+};
+
 //FUNCTIONS FOR MANIPULATE OR CREATE ELEMENTS
-function editElementClass(elementQuery = "", index = 0, newClass = "", removeThisClass = "") {
+function createNewElement(tag = "", text = "", elementClass = "", id = "") {
+  const newElement = document.createElement(tag);
+  newElement.textContent = text;
+  newElement.classList = elementClass;
+  newElement.id = id;
+  return newElement;
+}
+
+function editElementClass(elementQuery = "", index = 0, newClass = "", removeSomeClass = "") {
   const element = document.querySelectorAll(elementQuery);
   element[index].classList += ` ${newClass}`;
-  if (removeThisClass !== "") {
+  if (removeSomeClass !== "") {
     const stringOfTheClass = returnStringOfTheElementClass(elementQuery, index);
-    const classEdited = stringOfTheClass.replace(removeThisClass, "");
+    const classEdited = stringOfTheClass.replace(removeSomeClass, "");
     element[index].classList = classEdited;
   }
 }
@@ -40,4 +55,32 @@ function putRippleEfectsInTheIcones() {
   }
 }
 
+
+function putIconesAndSpanInTheDivInTheSmallHover() {
+  const div = document.querySelectorAll("#smallLeftHover div");
+  const divLength = div.length;
+  const icone = new Array(divLength), span = new Array(divLength);
+  for(let i = 0; i < divLength; i++) {
+    icone[i] = createNewElement("i", "", SmallLeftHoverOptions.classes[i]);
+    span[i] = createNewElement("span", SmallLeftHoverOptions.iconesName[i]);
+    div[i].appendChild(icone[i]);
+    div[i].appendChild(span[i]);
+  }
+}
+
+//FUNCTION OF INTERATIONS
+function manipulateTheLeftHoverByClick() {
+  if(bigHoverIsVisible === true) {
+    editElementClass("#leftHover", 0, "noVisibleElement");
+    editElementClass("#smallLeftHover", 0, "", "noVisibleElement");
+    bigHoverIsVisible = false;
+  } else {
+    editElementClass("#smallLeftHover", 0, "noVisibleElement");
+    editElementClass("#leftHover", 0, "", "noVisibleElement");
+    bigHoverIsVisible = true;
+  }
+}
+
+
 putRippleEfectsInTheIcones();
+putIconesAndSpanInTheDivInTheSmallHover();
